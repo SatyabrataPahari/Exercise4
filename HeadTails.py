7e83cdc53ab2e3141331ed42c7c024b0862ed987
ecommerceproject389@gmail.com <ben10satya10@gmail.com>
2022-07-10 01:15:36 +0900

    User name added

--- HeadTails.py
+++ HeadTails.py
@@ -1,10 +1,18 @@
 import random
 
 def main():
+    name = input("Who are you? ")
+    print ("Hello",name)
     i=1
+    heads=0
+    tails = 0
     while i<=3:
-        print("Round ",i, head_tail())
+        choice = head_tail()
+        print("Round",i, choice)
+        if choice == "Heads": heads+=1
+        else: tails+=1
         i+=1
+    print ("Heads:",heads,", Tails:",tails)
 
 
 def head_tail():
